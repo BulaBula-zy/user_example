984d85e1f8b3acf27d7953eee872406e697e582b
BulaBula_zy <1739634190@qq.com>
2024-02-02 22:56:47 +0800

    fix(todo-comments): add plugin todo-comments

--- mappings.lua
+++ mappings.lua
@@ -32,6 +32,12 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- open TODOs in telescope
+    ["<leader>T"] = {
+      "<cmd>TodoTelescope<cr>",
+      desc = "Open TODOs in Telescope",
+    },
   },
   t = {
     -- setting a mapping to false will disable it
--- plugins/user.lua
+++ plugins/user.lua
@@ -16,4 +16,18 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
+  -- TODO: test the plugin below
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {}, -- must use this code to setup
+    event = "User AstroFile", -- triggered after opening a file
+    cmd = { "TodoQuickFix" }, -- triggered after excuting :TodoQuickFix
+    key = {
+      "<leader>T",
+      "<cmd>TodoTelescope<cr>",
+      desc = "Open TODOs in Telescope",
+    }, -- triggered after inputing <leader>T
+  },
 }
