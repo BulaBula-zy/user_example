fd75aeaa8da9f3fff46a5fbc1927530b6bbcf914
BulaBula_zy <1739634190@qq.com>
2024-02-02 23:02:58 +0800

    fix(flash): add plugin flash

--- plugins/user.lua
+++ plugins/user.lua
@@ -30,4 +30,17 @@ return {
       desc = "Open TODOs in Telescope",
     }, -- triggered after inputing <leader>T
   },
+
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
 }
