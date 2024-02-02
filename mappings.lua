a24dd9d47be36a4431912387aac5aa86219839f1
BulaBula_zy <1739634190@qq.com>
2024-02-02 22:58:47 +0800

    fix(mapping): add mapping for quick save

--- mappings.lua
+++ mappings.lua
@@ -30,8 +30,9 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
 
     -- open TODOs in telescope
     ["<leader>T"] = {
