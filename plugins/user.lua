4f4a001e32a678dc0873c6a31fd53bc6e9b9d4c6
BulaBula_zy <1739634190@qq.com>
2024-02-02 22:51:10 +0800

    doc: add comments about lazy events function

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,10 @@
+-- Lazy Events
+--  User AstroFile: Triggered after opening a file
+--  VeryLazy: Triggered after starting Neovim
+--  BufEnter *.lua: Triggered after opening a lua file
+--  InsertEnter: Triggered after entering insert mode
+--  LspAttach: Triggered after starting LSPs
+
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
