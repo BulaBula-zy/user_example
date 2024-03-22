78ce2139d4a785963750484acb22673fe163c5a1
BulaBula_zy <1739634190@qq.com>
2024-03-22 17:32:38 +0800

    chore(plugins): change plugin directory structure

---
+++ plugins/todo-comments.lua
@@ -0,0 +1,13 @@
+return {
+  -- TODO: test the plugin below
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {}, -- must use this code to setup
+  event = "User AstroFile", -- triggered after opening a file
+  cmd = { "TodoQuickFix" }, -- triggered after excuting :TodoQuickFix
+  key = {
+    "<leader>T",
+    "<cmd>TodoTelescope<cr>",
+    desc = "Open TODOs in Telescope",
+  }, -- triggered after inputing <leader>T
+}
